07a1d2d3204af16ab7f1f8623e6cd1868bd16d19
Bwa23 <bwa23@cam.ac.uk>
2022-02-04 17:21:10 +0000

    Task 1C false commit

--- Task1C.py
+++ Task1C.py
@@ -8,8 +8,12 @@ from floodsystem.geo import stations_within_radius
 # Build the list of stations
 stations = build_station_list()
 
-#Use this list as the input for the range checking and alphabetic sorting function
+#Use this list as the input for the range checking function
 
-stations_within_range_sorted = stations_within_radius(stations, (52.2053, 0.1218), 10)
+stations_within_range_unsorted = stations_within_radius(stations, (52.2053, 0.1218), 10)
+
+#sort the unsorted list alphabetically and print result
+
+stations_within_range_sorted = sorted(stations_within_range_unsorted)
 
 print(stations_within_range_sorted)
