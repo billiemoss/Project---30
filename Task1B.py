ae0a74b5793b34771aeeebcd3cfc088743f68c34
Bwa23 <bwa23@cam.ac.uk>
2022-02-04 15:55:33 +0000

    Draft Complete Task 1B

---
+++ Task1B.py
@@ -0,0 +1,22 @@
+from floodsystem.stationdata import build_station_list
+
+from floodsystem.geo import stations_by_distance
+
+
+"""Requirements for Task 1B"""
+
+# Build list of stations
+stations = build_station_list()
+
+    #generates full stations by distance list
+station_distances = stations_by_distance(stations, (52.2053, 0.1218))
+
+    #indexes for first and last 10
+first_10 = station_distances[:10]
+last_10 = station_distances[-10:]
+
+    #prints the output 
+print(first_10)
+print(last_10)
+
+
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,16 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+
+def stations_by_distance(stations, p):
+   
+
+   station_distance = []
+   for station in stations:
+      distance = haversine(station.coord, p)
+      station_distance.append((station.name, station.town, distance))
+   #sort by distance
+   station_distance = sorted_by_key(station_distance, 2)
+   return station_distance
