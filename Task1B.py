f65b7ac7caf678b3315136b3f8d4ab7b90de8a0f
Bwa23 <bwa23@cam.ac.uk>
2022-02-04 16:46:21 +0000

    Task 1B complete

--- Task1B.py
+++ Task1B.py
@@ -5,18 +5,18 @@ from floodsystem.geo import stations_by_distance
 
 """Requirements for Task 1B"""
 
-# Build list of stations
+# Build the list of stations
 stations = build_station_list()
 
-    #generates full stations by distance list
+    #Use this list as the input for distance calculating and sorting function
 station_distances = stations_by_distance(stations, (52.2053, 0.1218))
 
-    #indexes for first and last 10
-first_10 = station_distances[:10]
-last_10 = station_distances[-10:]
+    #indexes for first and last 10 (sorted by distance)
+closest_10 = station_distances[:10]
+furthest_10 = station_distances[-10:]
 
     #prints the output 
-print(first_10)
-print(last_10)
+print(closest_10)
+print(furthest_10)
 
 
