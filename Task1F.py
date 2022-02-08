883cdd145ade2997c911f138a0ae435f1e2c57d5
Billie Moss <bjm69@cam.ac.uk>
2022-02-08 04:28:49 +0000

    Task 1F

---
+++ Task1F.py
@@ -0,0 +1,7 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
+
+stations = build_station_list()
+inconsistent_stations = inconsistent_typical_range_stations(stations)
+ordered_inconsistent_stations = sorted(inconsistent_stations)
+print(ordered_inconsistent_stations)
