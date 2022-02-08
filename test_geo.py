e72e0ce91e9c0bad635419593e94938cc943d081
Bwa23 <bwa23@cam.ac.uk>
2022-02-08 11:27:41 +0000

    First few Tests with Haversine, Sorter_by_key installed

--- test_geo.py
+++ test_geo.py
@@ -1,4 +1,7 @@
 
+from .utils import sorted_by_key  # noqa
+from  haversine import haversine
+
 from floodsystem.geo import stations_by_distance, stations_within_radius, rivers_with_station, stations_by_river, rivers_by_station_number 
 from floodsystem.stationdata import build_station_list
 
