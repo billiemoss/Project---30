286178ca16ea3b8de27b39425bc94a566b145590
Bwa23 <bwa23@cam.ac.uk>
2022-02-28 20:29:51 +0000

    Task 2B

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,15 +1,23 @@
-from station import MonitoringStation
+from .station import MonitoringStation
 
 
 
 #Task 2B - function which orders stations by relative water level
-def stations_level_over_threshold(stations, tol):
-    list_of_stations_over_threshold = []
+
+def stations_levels_over_threshold(stations, tol):
+    output = []
     for station in stations:
-        if tol <= MonitoringStation().relative_water_level() and MonitoringStation().typical_range_consistent() == True:
-            list_of_stations_over_threshold.append(MonitoringStation.name, MonitoringStation().relative_water_level())
-    sorted_list_of_stations_over_threshold = sorted(list_of_stations_over_threshold, key=lambda tup: tup[1], reverse=True)
-    return sorted_list_of_stations_over_threshold
+        rel_level = station.relative_water_level()
+        try:
+            if rel_level > tol:
+                output.append((station, station.relative_water_level()))
+        except:
+            pass
+    return sorted(output, key=lambda x: x[0].relative_water_level(), reverse=True)
 
 
 
+#Task 2C - function which returns ordered list of the N stations with highest relative level
+def stations_highest_rel_level(stations, N):
+    stations = [station for station in stations if station.relative_water_level() != None]
+    return sorted(stations, key=lambda x: x.relative_water_level(), reverse=True)[:N]
